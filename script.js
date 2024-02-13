cda0ec587e845b7397ba5fa6280c64476be73faf
hranto657 <hrant.simonyan.2018@gmail.com>
2024-02-13 21:32:05 +0400

    corrected again the written code

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
+const main = document.createElement("div");
+main.classList.add("main");
+document.body.appendChild(main);
+
+const headerBlock = document.createElement("div");
+headerBlock.classList.add("header-block");
+main.appendChild(headerBlock);
+
+const mainBlock = document.createElement("div");
+mainBlock.classList.add("main-block");
+main.appendChild(mainBlock);
+
 const headerTitle = document.createElement("h1");
 headerTitle.classList.add("header-title");
 headerTitle.innerHTML = "My Online Shop";
+headerBlock.appendChild(headerTitle);
 
 const icon = document.createElement("img");
 icon.classList.add("class", "icon");
 icon.setAttribute("src", "/images/shop_icon.png");
 icon.setAttribute("alt", "Online Shop Icon");
-
-const headerBlock = document.createElement("div");
-headerBlock.classList.add("header-block");
-headerBlock.appendChild(headerTitle);
 headerBlock.appendChild(icon);
-
-const mainBlock = document.createElement("div");
-mainBlock.classList.add("main-block");
-
-const main = document.createElement("div");
-main.classList.add("main");
-main.appendChild(headerBlock);
-main.appendChild(mainBlock);
-
-document.body.appendChild(main);
 
 async function getShopItems() {
   const response = await fetch("./shopItem.json");
@@ -35,20 +34,20 @@ function drawItems(data) {
   data.forEach((item) => {
     const shopItem = document.createElement("div");
     shopItem.classList.add("class", "shop-item");
-    
+
     const shopItemImage = document.createElement("img");
     shopItemImage.setAttribute("src", item.image);
     shopItemImage.setAttribute("alt", item.alt);
     shopItem.appendChild(shopItemImage);
-    
+
     const shopItemTitle = document.createElement("h2");
     shopItemTitle.innerHTML = item.title;
     shopItem.appendChild(shopItemTitle);
-    
+
     const shopItemDescription = document.createElement("p");
     shopItemDescription.innerHTML = item.description;
     shopItem.appendChild(shopItemDescription);
-    
+
     mainBlock.appendChild(shopItem);
   });
 }
